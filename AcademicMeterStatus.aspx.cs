e34e9dab85b7afbebabbd15cf55a41927d2ab1d7
Inderpal Singh <Inderpals@iiitd.ac.in>
2013-09-10 12:59:42 +0530

    Separate pages for meter status and link updates

---
+++ AcademicMeterStatus.aspx.cs
@@ -0,0 +1,499 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using App_Code.FetchingEnergySmap;
+using App_Code.Utility;
+
+
+public partial class MeterStatus : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DrawStatus();
+    }
+
+
+    protected void DrawStatus()
+    {
+        popup.Attributes.Add("class", "abstract");
+       
+        DrawAcademic();
+    }
+
+    protected void DrawFaculty()
+    {
+        fac.Controls.Clear();
+        string building = "Faculty Housing";
+        string[] allFloors;
+
+        HtmlGenericControl facultyDiv = new HtmlGenericControl("div");
+        facultyDiv.ID = "grlDiv";
+        facultyDiv.Attributes.Add("class", "containers");
+
+        HtmlGenericControl heading = new HtmlGenericControl("h1");
+
+        heading.InnerHtml = "Faculty Housing";
+        facultyDiv.Controls.Add(heading);
+
+        FetchEnergyDataS_Map.ListingFloors(building, out allFloors);
+        var allFloor = allFloors.OrderBy(o => int.Parse(o.ToString()));
+        foreach(string kfloor in allFloor)
+        {
+            string[] allMeters;
+
+            FetchEnergyDataS_Map.ListingMeter(building, kfloor, out allMeters);
+
+            bool status=false;
+
+        if (allMeters != null)
+        {
+
+            HtmlGenericControl subHeading = new HtmlGenericControl("h4");
+
+            subHeading.InnerHtml = "Floor: "+kfloor;
+            facultyDiv.Controls.Add(subHeading);
+
+
+            HtmlGenericControl hr1 = new HtmlGenericControl("hr");
+            facultyDiv.Controls.Add(hr1);
+
+            for (int i = 0; i < allMeters.Length; i++)
+            {
+                FetchEnergyDataS_Map.PingingMeter(null, allMeters[i], out status);
+
+                HtmlGenericControl meterIdSpan = new HtmlGenericControl("span");
+                meterIdSpan.Attributes.Add("class", "meterSpan");
+
+                Button meterIdLabel = new Button();
+                meterIdLabel.ID = "meterTick" + i.ToString()+kfloor;
+                meterIdLabel.Text = allMeters[i];
+
+                //HtmlGenericControl meterIdLabel = new HtmlGenericControl("h2");
+                //meterIdLabel.InnerHtml = allMeters[i];
+                meterIdLabel.Attributes.Add("val", allMeters[i]);
+                meterIdLabel.Style.Add("background-color", "red");
+                meterIdLabel.Attributes.Add("class", "meterLabel");
+                meterIdLabel.Click += new EventHandler(LastSeenAt);
+
+                if (status == true)
+                {
+                    meterIdLabel.Style.Add("background-color", "green");
+                }
+
+                meterIdSpan.Controls.Add(meterIdLabel);
+                facultyDiv.Controls.Add(meterIdSpan);
+            }
+
+        }
+
+        } fac.Controls.Add(facultyDiv);
+
+    }
+
+    protected void DrawGirlsHostel()
+    {
+        grl.Controls.Clear();
+        string building = "Girls Hostel";
+        string[] allFloors;
+
+        HtmlGenericControl facultyDiv = new HtmlGenericControl("div");
+        facultyDiv.ID = "facDiv";
+        facultyDiv.Attributes.Add("class", "containers");
+
+        HtmlGenericControl heading = new HtmlGenericControl("h1");
+
+        heading.InnerHtml = "Girls Hostel";
+        facultyDiv.Controls.Add(heading);
+
+        FetchEnergyDataS_Map.ListingFloors(building, out allFloors);
+        var allFloor = allFloors.OrderBy(o => int.Parse(o.ToString()));
+        foreach(string kfloor in allFloor)
+        {
+            string[] allMeters;
+
+            FetchEnergyDataS_Map.ListingMeter(building, kfloor, out allMeters);
+            bool status = false;
+
+        if (allMeters != null)
+        {
+            HtmlGenericControl subHeading = new HtmlGenericControl("h4");
+
+            subHeading.InnerHtml = "Floor: " + kfloor;
+            facultyDiv.Controls.Add(subHeading);
+
+
+            HtmlGenericControl hr1 = new HtmlGenericControl("hr");
+            facultyDiv.Controls.Add(hr1);
+
+            for (int i = 0; i < allMeters.Length; i++)
+            {
+                FetchEnergyDataS_Map.PingingMeter(null, allMeters[i], out status);
+
+                HtmlGenericControl meterIdSpan = new HtmlGenericControl("span");
+                meterIdSpan.Attributes.Add("class", "meterSpan");
+
+                Button meterIdLabel = new Button();
+                meterIdLabel.ID = "meterTickGH" + i.ToString()+kfloor;
+                meterIdLabel.Text = allMeters[i];
+
+                //HtmlGenericControl meterIdLabel = new HtmlGenericControl("h2");
+                //meterIdLabel.InnerHtml = allMeters[i];
+                meterIdLabel.Attributes.Add("val", allMeters[i]);
+                meterIdLabel.Style.Add("background-color", "red");
+                meterIdLabel.Attributes.Add("class", "meterLabel");
+                meterIdLabel.Click += new EventHandler(LastSeenAt);
+
+                if (status == true)
+                {
+                    meterIdLabel.Style.Add("background-color", "green");
+                }
+
+                meterIdSpan.Controls.Add(meterIdLabel);
+                facultyDiv.Controls.Add(meterIdSpan);
+            }
+
+
+        } grl.Controls.Add(facultyDiv);
+        }
+
+    }
+
+    protected void DrawBoysHostel()
+    {
+        boy.Controls.Clear();
+        string building = "Boys Hostel";
+        string[] allFloors;
+
+        HtmlGenericControl facultyDiv = new HtmlGenericControl("div");
+        facultyDiv.ID = "boyDiv";
+        facultyDiv.Attributes.Add("class", "containers");
+
+        HtmlGenericControl heading = new HtmlGenericControl("h1");
+
+        heading.InnerHtml = "Boys Hostel";
+        facultyDiv.Controls.Add(heading);
+
+        FetchEnergyDataS_Map.ListingFloors(building, out allFloors);
+        var allFloor = allFloors.OrderBy(o => int.Parse(o.ToString()));
+        foreach(string kfloor in allFloor)
+        {
+            string[] allMeters;
+
+            FetchEnergyDataS_Map.ListingMeter(building, kfloor, out allMeters);
+            bool status = false;
+
+            if (allMeters != null)
+            {
+
+                HtmlGenericControl subHeading = new HtmlGenericControl("h4");
+
+                subHeading.InnerHtml = "Floor: " + kfloor;
+                facultyDiv.Controls.Add(subHeading);
+
+                HtmlGenericControl hr1 = new HtmlGenericControl("hr");
+                facultyDiv.Controls.Add(hr1);
+
+                for (int i = 0; i < allMeters.Length; i++)
+                {
+                    FetchEnergyDataS_Map.PingingMeter(null, allMeters[i], out status);
+
+                    HtmlGenericControl meterIdSpan = new HtmlGenericControl("span");
+                    meterIdSpan.Attributes.Add("class", "meterSpan");
+
+                    Button meterIdLabel = new Button();
+                    meterIdLabel.ID = "meterTickBH" + i.ToString() + kfloor;
+                    meterIdLabel.Text = allMeters[i];
+
+                    //HtmlGenericControl meterIdLabel = new HtmlGenericControl("h2");
+                    //meterIdLabel.InnerHtml = allMeters[i];
+                    meterIdLabel.Attributes.Add("val", allMeters[i]);
+                    meterIdLabel.Style.Add("background-color", "red");
+                    meterIdLabel.Attributes.Add("class", "meterLabel");
+                    meterIdLabel.Click += new EventHandler(LastSeenAt);
+
+                    if (status == true)
+                    {
+                        meterIdLabel.Style.Add("background-color", "green");
+                    }
+
+                    meterIdSpan.Controls.Add(meterIdLabel);
+                    facultyDiv.Controls.Add(meterIdSpan);
+                }
+
+            }
+            boy.Controls.Add(facultyDiv);
+        }
+
+    }
+
+    protected void DrawAcademic()
+    {
+        acd.Controls.Clear();
+        string building = "Academic Building";
+        string[] allFloors;
+
+            HtmlGenericControl facultyDiv = new HtmlGenericControl("div");
+            facultyDiv.ID = "acdDiv";
+            facultyDiv.Attributes.Add("class", "containers");
+
+            HtmlGenericControl heading = new HtmlGenericControl("h1");
+            heading.InnerHtml = "Academic Building";
+            facultyDiv.Controls.Add(heading);
+
+        FetchEnergyDataS_Map.ListingFloors(building, out allFloors);
+        var allFloor = allFloors.OrderBy(o => int.Parse(o.ToString()));
+        foreach(string kfloor in allFloor)
+        {
+            string[] allMeters;
+
+            FetchEnergyDataS_Map.ListingMeter(building, kfloor, out allMeters);
+            bool status = false;
+
+            if (allMeters != null)
+            {
+                HtmlGenericControl subHeading = new HtmlGenericControl("h4");
+                subHeading.InnerHtml = "Floor: "+kfloor;
+                facultyDiv.Controls.Add(subHeading);
+
+                    FetchEnergyDataS_Map.ListingMeter(building, kfloor, out allMeters);
+                    HtmlGenericControl hr1 = new HtmlGenericControl("hr");
+                    facultyDiv.Controls.Add(hr1);
+
+                    for (int i = 0; i < allMeters.Length; i++)
+                    {
+                        FetchEnergyDataS_Map.PingingMeter(null, allMeters[i], out status);
+
+                        HtmlGenericControl meterIdSpan = new HtmlGenericControl("span");
+                        meterIdSpan.Attributes.Add("class", "meterSpan");
+
+                        Button meterIdLabel = new Button();
+                        meterIdLabel.ID = "meterTickAC" + i.ToString()+kfloor;
+                        meterIdLabel.Text = allMeters[i];
+
+                        //HtmlGenericControl meterIdLabel = new HtmlGenericControl("h2");
+                        //meterIdLabel.InnerHtml = allMeters[i];
+                        meterIdLabel.Attributes.Add("val", allMeters[i]);
+                        meterIdLabel.Style.Add("background-color", "red");
+                        meterIdLabel.Attributes.Add("class", "meterLabel");
+                        meterIdLabel.Click += new EventHandler(LastSeenAt);
+
+                        if (status == true)
+                        {
+                            meterIdLabel.Style.Add("background-color", "green");
+                        }
+
+                        meterIdSpan.Controls.Add(meterIdLabel);
+                        facultyDiv.Controls.Add(meterIdSpan);
+                    }
+
+                }
+                    acd.Controls.Add(facultyDiv);
+            }
+
+    }
+
+    protected void DrawLibrary()
+    {
+            lib.Controls.Clear();
+            string building = "Library Building";
+            string[] allFloors;
+
+            HtmlGenericControl facultyDiv = new HtmlGenericControl("div");
+            facultyDiv.ID = "libDiv";
+            facultyDiv.Attributes.Add("class", "containers");
+
+            HtmlGenericControl heading = new HtmlGenericControl("h1");
+
+            heading.InnerHtml = "Library Building";
+            facultyDiv.Controls.Add(heading);
+
+            FetchEnergyDataS_Map.ListingFloors(building, out allFloors);
+            var allFloor = allFloors.OrderBy(o => int.Parse(o.ToString()));
+            foreach (string kfloor in allFloor) 
+            {
+                string[] allMeters;
+
+                FetchEnergyDataS_Map.ListingMeter(building, kfloor, out allMeters);
+                bool status = false;
+
+                if (allMeters != null)
+                {
+                    HtmlGenericControl subHeading = new HtmlGenericControl("h4");
+                    subHeading.InnerHtml = "Floor: " + kfloor;
+                    facultyDiv.Controls.Add(subHeading);
+
+                    for (int i = 0; i < allMeters.Length; i++)
+                    {
+                        FetchEnergyDataS_Map.PingingMeter(null, allMeters[i], out status);
+
+                        HtmlGenericControl meterIdSpan = new HtmlGenericControl("span");
+                        meterIdSpan.Attributes.Add("class", "meterSpan");
+
+                        Button meterIdLabel = new Button();
+                        meterIdLabel.ID = "meterTickLB" + i.ToString() + kfloor;
+                        meterIdLabel.Text = allMeters[i];
+
+                        //HtmlGenericControl meterIdLabel = new HtmlGenericControl("h2");
+                        //meterIdLabel.InnerHtml = allMeters[i];
+                        meterIdLabel.Attributes.Add("val", allMeters[i]);
+                        meterIdLabel.Style.Add("background-color", "red");
+                        meterIdLabel.Attributes.Add("class", "meterLabel");
+                        meterIdLabel.Click += new EventHandler(LastSeenAt);
+                        if (status == true)
+                        {
+                            meterIdLabel.Style.Add("background-color", "green");
+                        }
+
+                        meterIdSpan.Controls.Add(meterIdLabel);
+                        facultyDiv.Controls.Add(meterIdSpan);
+                    }
+
+                }
+            }
+            lib.Controls.Add(facultyDiv);
+        
+    }
+
+    protected void DrawMess()
+    {
+        mess.Controls.Clear();
+        string building = "Mess Building";
+        string[] allFloors;
+
+        HtmlGenericControl facultyDiv = new HtmlGenericControl("div");
+        facultyDiv.ID = "messDiv";
+        facultyDiv.Attributes.Add("class", "containers");
+
+        HtmlGenericControl heading = new HtmlGenericControl("h1");
+
+        heading.InnerHtml = "Mess Building";
+        facultyDiv.Controls.Add(heading);
+
+         FetchEnergyDataS_Map.ListingFloors(building, out allFloors);
+        var allFloor = allFloors.OrderBy(o => int.Parse(o.ToString()));
+        foreach(string kfloor in allFloor)
+        {
+            string[] allMeters;
+
+            FetchEnergyDataS_Map.ListingMeter(building, kfloor, out allMeters);
+            bool status = false;
+
+        if (allMeters != null)
+        {
+         
+
+            HtmlGenericControl hr1 = new HtmlGenericControl("hr");
+            facultyDiv.Controls.Add(hr1);
+
+            for (int i = 0; i < allMeters.Length; i++)
+            {
+                FetchEnergyDataS_Map.PingingMeter(null, allMeters[i], out status);
+
+                HtmlGenericControl meterIdSpan = new HtmlGenericControl("span");
+                meterIdSpan.Attributes.Add("class", "meterSpan");
+
+                Button meterIdLabel = new Button();
+                meterIdLabel.ID = "meterTickMS" + i.ToString()+kfloor;
+                meterIdLabel.Text = allMeters[i];
+
+                //HtmlGenericControl meterIdLabel = new HtmlGenericControl("h2");
+                //meterIdLabel.InnerHtml = allMeters[i];
+                meterIdLabel.Attributes.Add("val", allMeters[i]);
+                meterIdLabel.Style.Add("background-color", "red");
+                meterIdLabel.Attributes.Add("class", "meterLabel");
+                meterIdLabel.Click += new EventHandler(LastSeenAt);
+
+                if (status == true)
+                {
+                    meterIdLabel.Style.Add("background-color", "green");
+                }
+
+                meterIdSpan.Controls.Add(meterIdLabel);
+                facultyDiv.Controls.Add(meterIdSpan);
+            }
+
+        }
+            mess.Controls.Add(facultyDiv);
+        }
+
+    }
+
+    protected void LastSeenAt(object sender, EventArgs e)
+    {
+        double val = 0;
+        double val2 = 0;
+        int time = 0;
+        string building = ""; string wing = ""; string floor = "";
+        string type = ""; string block = ""; string flat = "";
+        popup.Controls.Clear();
+        popup.Attributes.Add("class", "abstractH");
+
+        Button btn = (Button)sender;
+        string metId = btn.Text;
+
+       
+
+        FetchEnergyDataS_Map.GetMeterLocationByID(metId, out building, out floor, out wing, out flat, out block, out type);
+
+        HtmlGenericControl metertype = new HtmlGenericControl("p");
+        metertype.InnerHtml = "<p><font style='background-color:skyblue;line-height:20px;width:35px;padding:5px;text-align:center'>" + btn.Text + "</font>&nbsp;&nbsp;" + type + "&nbsp;<img src='images/closeButton.png' alt='close' width='20px' align='right'style='padding-right:10px;cursor:pointer;' class='clos'/></p></br>";
+        popup.Controls.Add(metertype);
+
+        FetchEnergyDataS_Map.GetMeterByID(metId,"Energy", out val, out time);
+        if (time > 0)
+        {
+            Utilities ut = Utilitie_S.EpochToDateTime(time);
+
+            HtmlGenericControl lastseen = new HtmlGenericControl("span");
+            lastseen.InnerHtml = "Last seen: " + ut.Date.ToString("HH:mm dd MMM");
+
+            FetchEnergyDataS_Map.GetMeterByID(metId, "Power", out val2, out time);
+
+            HtmlGenericControl readings = new HtmlGenericControl("span");
+            if (building != "Faculty Housing")
+            {
+                readings.InnerHtml = "<font style='font-size:small; line-height:25px;color:gray;font-weight:bolder;'><br />" + Math.Round(val / 1000, 2).ToString() + " KWh, " + Math.Round(val2, 2).ToString() + "W</font>";
+            }
+            popup.Controls.Add(lastseen);
+            popup.Controls.Add(readings);
+        }
+
+        if (building == "Boys Hostel" || building == "Girls Hostel")
+        {
+            HtmlGenericControl winger = new HtmlGenericControl("p");
+            winger.InnerHtml = "Wing: " + wing;
+            popup.Controls.Add(winger);
+        }
+        if (building == "Faculty Housing")
+        {
+            HtmlGenericControl flatter = new HtmlGenericControl("p");
+            flatter.InnerHtml = "Apartment: " + flat;
+            popup.Controls.Add(flatter);
+        }
+        if (building == "Academic Building")
+        {
+            HtmlGenericControl blocker = new HtmlGenericControl("p");
+            blocker.InnerHtml = "Block: " + block;
+            popup.Controls.Add(blocker);   
+        }
+        HtmlGenericControl floorer = new HtmlGenericControl("p");
+        floorer.InnerHtml = "Floor: " + floor;
+        popup.Controls.Add(floorer);
+
+        HtmlGenericControl builder = new HtmlGenericControl("p");
+        builder.InnerHtml = "Building: " + building;
+        popup.Controls.Add(builder);
+
+       
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        DrawStatus();
+    }
+
+
+}
--- UsefulLinks.aspx
+++ UsefulLinks.aspx
@@ -97,27 +97,31 @@
                <h3>Research Advice</h3>
                <p>
                    
-                   <a class="uselink" href="http://blizzard.cs.uwaterloo.ca/keshav/wiki/index.php/Aphorisms">Prof. Keshav’s Aphorisms</a> <br />Its a great collection of advice on everything related to research and some on life too. Its a must read for CS researchers.
+                   <a class="uselink" href="http://blizzard.cs.uwaterloo.ca/keshav/wiki/index.php/Aphorisms" target="_blank">Prof. Keshav’s Aphorisms</a> <br />Its a great collection of advice on everything related to research and some on life too. Its a must read for CS researchers.
                    <br />
-<a class="uselink" href="http://www.cs.berkeley.edu/~pattrsn/talks/BadCareer.pdf">How to Have a Bad Career in Research/Academia</a> - By Prof. David Patterson
+<a class="uselink" href="http://www.cs.berkeley.edu/~pattrsn/talks/BadCareer.pdf" target="_blank">How to Have a Bad Career in Research/Academia</a> - By Prof. David Patterson
 <br />
 It contains advice starting from being a graduate student till the post PhD period. It also has some links on good writing tips, giving a talk etc. If you are a researcher, you should definitely give this a read.
 <br />
-<a class="uselink" href="http://researchnoter.wordpress.com/2013/08/17/advice-to-a-young-scientist-e-w-dijkstra/">Advice to a Young Scientist</a> – By E.W.Dijkstra<br />
+<a class="uselink" href="http://researchnoter.wordpress.com/2013/08/17/advice-to-a-young-scientist-e-w-dijkstra/" target="_blank">Advice to a Young Scientist</a> – By E.W.Dijkstra<br />
 I covered the points made by EWD in this blog post. Its truly inspiring!
 <br />
-<a class="uselink" href="http://www.cs.cmu.edu/afs/cs.cmu.edu/user/mleone/web/how-to.html">Advice on Research and Writing</a> – By Mark Leone<br />
+<a class="uselink" href="http://www.cs.cmu.edu/afs/cs.cmu.edu/user/mleone/web/how-to.html" target="_blank">Advice on Research and Writing</a> – By Mark Leone<br />
 Its a good collection of advice on how to conduct research and how to communicate it. Its mostly for computer science researchers.
                </p>
                </div>
                 <div id="stat" runat="server">
                <h3>Statistical Links</h3><p>
-               <b style="color:skyblue">Reproducible Research:</b><br /> Similar to Mathematica Notebook, IPython Notebook allows one to integrate code, output, markdown and much more in the same document. These notebooks can be rendered as HTML, LaTeX and even in HTML5 based slideshows. Similarly, R has a package named Knitr, which allows similar stuff. Matlab too allows to publish your work. Our group is committed to document our code using these packages to make our research reproducible and more authentic, also releasing our code.
-<br />R also has a package called Shiny, which can be used for bootstrapping web applications in R.</p>
+               <b style="color:skyblue">Reproducible Research:</b><br /> Similar to <a target="_blank" href="http://reference.wolfram.com/legacy/v5/Tour/MathematicaNotebooks.html">Mathematica Notebook</a>, <a href="http://ipython.org/notebook.html" target="_blank">IPython Notebook</a> allows one to integrate code, output, markdown and much more in the same document. These notebooks 
+               can be rendered as <a href="http://en.wikipedia.org/wiki/HTML" target="_blank">HTML</a>, <a href="http://www.latex-project.org/" target="_blank">LaTeX</a> and even in <a href="http://en.wikipedia.org/wiki/HTML5" target="_blank">HTML5</a> based slideshows. 
+               <br />
+               Similarly, <a href="http://en.wikipedia.org/wiki/R_(programming_language)" target="_blank">R</a> has a package named <a href="http://yihui.name/knitr/" target="_blank">Knitr</a>, which allows similar stuff. <a href="http://www.mathworks.in/products/matlab/" target="_blank">Matlab</a> too allows to publish your work. 
+               Our group is committed to document our code using these packages to make our research reproducible and more authentic, also releasing our code.
+<br />R also has a package called <a href="http://shiny.rstudio.org/" target="_blank">Shiny</a>, which can be used for bootstrapping web applications in R.</p>
                </div>
                 <div id="tech" runat="server">
                <h3>Technical Writing</h3>
-               <p><a class="uselink" href="http://matt.might.net/articles/shell-scripts-for-passive-voice-weasel-words-duplicates/">Matt Might's shell scripts to improve writing:</a><br />
+               <p><a class="uselink" href="http://matt.might.net/articles/shell-scripts-for-passive-voice-weasel-words-duplicates/" target="_blank">Matt Might's shell scripts to improve writing:</a><br />
                Three scripts developed by Matt Might in course of his experience in reviewing and writing papers. These scripts help in finding passive voice usage (which if not done properly, may lead to misplacement of emphasis), to remove lexical illusions (such as consecutive 'the') and to remove weasel words such as very, quite, etc., which we often use to spice up our work, but, are not required.</p>
                </div>
               
